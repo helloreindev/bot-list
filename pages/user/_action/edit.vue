5e7dec28c6b2a04b03334c1c590791faf1b014d4
Reinhardt <contact@reinhello.xyz>
2022-05-02 16:24:17 +0700

    Add main route page

---
+++ pages/user/_action/edit.vue
@@ -0,0 +1,14 @@
+<template>
+  <ProfileEditComponent />
+</template>
+
+<script>
+import ProfileEditComponent from "~/components/User/ProfileEditComponent.vue";
+
+export default {
+  name: "UserEditProfileLayout",
+  components: { ProfileEditComponent },
+};
+</script>
+
+<style></style>
---
+++ pages/user/_user.vue
@@ -0,0 +1,14 @@
+<template>
+  <Profile />
+</template>
+
+<script>
+import Profile from "~/components/User/ProfileComponent.vue";
+
+export default {
+  name: "UserProfilePage",
+  components: { Profile },
+};
+</script>
+
+<style></style>
