524eb242e0e79498f35f5dbf9b1b865fd392635f
Reinhardt <contact@reinhello.tech>
2022-05-02 16:24:17 +0700

    Add main route page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Home />
+</template>
+
+<script>
+import Home from "~/components/HomeComponent.vue";
+
+export default {
+  name: "IndexPage",
+  components: { Home },
+};
+</script>
---
+++ pages/me.vue
@@ -0,0 +1,52 @@
+<template>
+  <center>
+    <div v-if="loading"></div>
+    <div v-else-if="!loggedIn">
+      <h3 style="margin-top: 10%">
+        It seems you're not authorised yet. Please click the <b>Login</b> button
+        below to authorise yourself.
+      </h3>
+      <button @click="redirectLogin"><span class="text">Login</span></button>
+    </div>
+  </center>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ProfilePage",
+  data() {
+    return {
+      loading: true,
+      loggedIn: false,
+      user: {},
+    };
+  },
+  mounted() {
+    this.$nextTick(async () => {
+      await this.fetchSelfUser();
+      await this.fetchLogin();
+    });
+  },
+  methods: {
+    fetchLogin() {
+      if (this.loggedIn) {
+        this.$router.push(`/user/${this.user.id}`);
+      }
+    },
+    async fetchSelfUser() {
+      await axios.get(`http://localhost:3000/api/v1/users/@me`).then((req) => {
+        this.loggedIn = true;
+        this.loading = false;
+        this.user = req.data.user;
+      });
+    },
+    redirectLogin() {
+      location.href = "/login?backUrl=me";
+    },
+  },
+};
+</script>
+
+<style></style>
