d0ac0051744240a099274a3d85fb3b9ea7b9492b
Reinhardt <contact@reinhello.xyz>
2022-05-09 15:30:40 +0700

    Add function to create bot mongoose model

--- api/CreateMongooseModel.ts
+++ api/CreateMongooseModel.ts
@@ -1,7 +1,14 @@
 /* eslint-disable import/named */
 
 import { Connection } from "mongoose";
+import { Bot, createBotModel } from "./Models/BotModel";
 import { createUserModel, User } from "./Models/UserModel";
+
+export function createBotMongooseModel(connection: Connection, body: Bot) {
+  const model = createBotModel(connection);
+
+  return model.create(body);
+}
 
 export function createUserMongooseModel(connection: Connection, body: User) {
   const model = createUserModel(connection);
