ed71d0300b5b406a513b8e8f6c07bf559bee8eb7
Reinhardt <contact@reinhello.xyz>
2022-05-09 15:31:46 +0700

    Add `Endpoints` class

---
+++ api/Managers/Endpoints.ts
@@ -0,0 +1,9 @@
+export const API_VERSION = 1;
+export const API_URL = `/api/v${API_VERSION}`;
+
+export const Endpoints = {
+    Callback: "/callback",
+    Login: "/login",
+    Logout: "/logout",
+    User: (userID: string) => `${API_URL}/users/${userID}`
+}
