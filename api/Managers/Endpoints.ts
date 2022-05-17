87ddbfeb090dcea0d028a4272f61d0db7eac7249
Reinhardt <contact@reinhello.tech>
2022-05-17 07:23:38 +0700

    Update endpoint

--- api/Managers/Endpoints.ts
+++ api/Managers/Endpoints.ts
@@ -2,6 +2,7 @@ export const API_VERSION = 1;
 export const API_URL = `/api/v${API_VERSION}`;
 
 export const Endpoints = {
+  Bot: (botID: string) => `${API_URL}/bots/${botID}`,
   Callback: "/callback",
   Login: "/login",
   Logout: "/logout",
