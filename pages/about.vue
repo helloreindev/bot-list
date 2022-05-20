dbfab5492fade2682b00564221956ed4555a2cab
Reinhardt <contact@reinhello.xyz>
2022-05-20 15:07:13 +0700

    Add about page

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <AboutComponent />
+</template>
+
+<script>
+import AboutComponent from "~/components/AboutComponent.vue";
+
+export default {
+  name: "AboutPage",
+  components: { AboutComponent },
+};
+</script>
+
+<style></style>
